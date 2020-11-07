7eae10671d0e669e11211a4da00768669278fcb4
Deepti Verma <deeptiverma455@gmail.com>
2020-11-08 02:56:23 +0530

    changess

--- src/components/index.js
+++ src/components/index.js
@@ -12,21 +12,21 @@ function updateStructure(rec1,rec2){
 		rec2[left]=rect1[left]-rec2[left];
 		rec2[width]=rect1[width]-rec2[width];
 		rec2[height]=rect1[height]-rec2[height];
-		return rec2;
+		// return rec2;
 	}else if(rec2[top]<rec1[top] && rec2[height]<rec1[height] && rec2[left]<rec1[left] && rec2[width]<rec1[width]){
 		rec1[top]=rec2[top]-rect1[top];
 		rec1[left]=rec2[left]-rect1[left];
 		rec1[width]=rec2[width]-rect1[width];
 		rec1[height]=rec2[height]-rect1[height];
-		return rec1;
+		// return rec1;
 	}else if(rec1[top]==rec2[top] && rect1[height]==rec2[height] && rect1[left]==rec2[left] && rec1[width]==rec2[width]){
 		rec1[top]=rec2[top]-rect1[top];
 		rec1[left]=rec2[left]-rect1[left];
 		rec1[width]=rec2[width]-rect1[width];
 		rec1[height]=rec2[height]-rect1[height];
-		return rect1;
+		// return rect1;
 	}else{
-		return "none";
+		// return "none";
 	}
 
 }
